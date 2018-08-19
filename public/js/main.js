74cf232d085fd30a3f6fc7a42aa85ca888210b44
Roman <starscreemm8@gmail.com>
2018-08-19 22:25:46 +0300

    add drag and drop

--- public/js/main.js
+++ public/js/main.js
@@ -10,11 +10,20 @@ document.body.appendChild(renderer.domElement);
 var controls = new THREE.OrbitControls( camera );
 
 
-var planeGeometry = new THREE.PlaneGeometry(60, 40, 10, 10);
-var planeMaterial = new THREE.MeshLambertMaterial({color: 0x4553c1, side: THREE.DoubleSide, wireframe: true});
-var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+var horPlaneGeometry = new THREE.PlaneBufferGeometry(6, 6, 10, 10);
+var horPlaneMaterial = new THREE.MeshLambertMaterial({color: 0x4553c1, transparent:true, opacity:.1, side: THREE.DoubleSide});
+var horPlane = new THREE.Mesh(horPlaneGeometry, horPlaneMaterial);
 
-scene.add(plane);
+scene.add(horPlane);
+
+// var verPlaneGeometry = new THREE.PlaneGeometry(6000, 6000, 10, 10);
+// var verPlaneMaterial = new THREE.MeshLambertMaterial({color: 0x454545, transparent: true, side: THREE.DoubleSide});
+// var verPlane = new THREE.Mesh(verPlaneGeometry, verPlaneMaterial);
+// verPlane.rotation.y = Math.PI / 2;
+
+//scene.add(verPlane);
+
+//var planes = [horPlane, verPlane];
 
 var axes = new THREE.AxesHelper( 5 );
 scene.add( axes  );
@@ -28,6 +37,7 @@ var light = new THREE.AmbientLight( 0xffffff ); // soft white light
 scene.add( light );
 var loader = new THREE.OBJLoader();
 var models = [];
+var obj;
 var geometry;
 var vertices = [];
 var distance = [];
@@ -43,8 +53,10 @@ loader.load(
             model.material.color = new THREE.Color(0xcccccc);
             model.material.wireframe = true;
             models.push(model);
-            geometry = new THREE.Geometry().fromBufferGeometry(model.geometry);
-            vertices = geometry.vertices;
+            model.geometry = new THREE.Geometry().fromBufferGeometry(model.geometry);
+            obj = model;
+            vertices = model.geometry.vertices;
+            //console.log(vertices);
         }
     });
     scene.add(object);
@@ -70,22 +82,38 @@ var mouse = new THREE.Vector2();
 
 function onMouseClick( e ) {
     controls.enabled = true;
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+	mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
     
     
     raycaster.setFromCamera( mouse, camera );
 
     var intersects = raycaster.intersectObjects( models );
+    //console.log(obj);
     if(intersects.length !== 0 && intersects[0].object.name === 'point') {
         controls.enabled = false;
-        var newCoords = new THREE.Vector2();
-        window.onmousemove = function() {
-            newCoords = onDrag(newCoords);
-            raycaster.setFromCamera(newCoords, camera);
-            // var raycaster = new THREE.Raycaster(camera.position,
-            // newCoords.sub(camera.position).normalize());
-            console.log(newCoords.sub(camera.position).normalize());
+        var vector = new THREE.Vector2();
+        var SELECTED = intersects[0].object;
+        window.onmousemove = function(e) {
+            //newCoords = onDrag(newCoords);
+            vector.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+            vector.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+            //this.console.log(vector);
+            raycaster.setFromCamera(vector, camera);
+
+            horPlane.position.copy(point.position);
+            horPlane.lookAt(camera.position);
+            intersects = raycaster.intersectObject(horPlane);
+            point.position.copy(intersects[0].point);
+            if(obj)
+            {
+                obj.geometry.vertices[0].copy(point.position);
+                this.console.log(obj.geometry.vertices[0])
+                obj.geometry.verticesNeedUpdate=true;
+            }
+            //obj.geometry.vertices[0].copy(point.position);
+            //SELECTED.geometry.verticesNeedUpdate=true;
+            //this.console.log(point.position);
         }
 
         window.onmouseup = function(e) {
@@ -107,7 +135,7 @@ function onMouseClick( e ) {
         vertices.sort(function(a, b) {
             return a.distance - b.distance;
         });
-
+        obj = intersects[0].object;
         point = new THREE.Mesh( new THREE.SphereGeometry(0.1), new THREE.MeshBasicMaterial( { color: 0xff0000} ));
         point.position.set(vertices[0].x, vertices[0].y, vertices[0].z);
         point.name = 'point';
@@ -117,14 +145,14 @@ function onMouseClick( e ) {
 
 }
 
-function onDrag(mouse) {
+// function onDrag(mouse) {
 
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+//     mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+//     mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
-    return mouse;
+//     return mouse;
 
-}
+// }
 
 function render() {
 
