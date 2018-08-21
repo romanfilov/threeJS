12bcdf35e8c216999229155e2a6319f1c74c1be2
romanfilov <romanfilov96@gmail.com>
2018-08-21 15:13:01 +0300

    "drag and drop finished"

--- public/js/main.js
+++ public/js/main.js
@@ -70,7 +70,6 @@ loader.load(
 
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
-<<<<<<< HEAD
 var points = [];
 var geometry;
 var index;
@@ -111,93 +110,9 @@ function onMouseClick(e) {
             points.push(point);
             scene.add(point);
         }
-=======
-var geometry;
-var index;
-// function onMouseMove(e) {
-//     e.preventDefault();
-//     window.onmousemove = function() {
-//         this.removeEventListener('click', onMouseClick);
-//     }
-//     window.onmouseup = function() {
-//         this.onmousemove = null;
-//     }
-//     window.addEventListener('click', onMouseClick);
-// }
-
-function onMouseClick( e ) {
-	mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
-    
-    raycaster.setFromCamera( mouse, camera );
-    var intersects = raycaster.intersectObjects( models );
-    console.log(intersects);
-    if( intersects.length !== 0 && intersects[0].object.name == 'point' ) {
-        controls.enabled = false;
-        var vector = new THREE.Vector3();
-        var ray = new THREE.Raycaster();
-        var crossing;
-        window.onmousemove = function(e) {
-            vector.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-            vector.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
-            vector.z = 0.5;
-
-            ray.setFromCamera(vector, camera);
-
-            horPlane.position.copy(intersects[0].object.position);
-            horPlane.lookAt(camera.position);
-            crossing = ray.intersectObject(horPlane);
-            point.position.copy(crossing[0].point);
-            
-            geometry.vertices[index].copy(point.position);
-
-            geometry.verticesNeedUpdate = true;
-            //this.console.log(point.position);
-        }
-
-        window.onmouseup = function(e) {
-            this.onmousemove = null;
-            this.onmouseup = null;
-            geometry.verticesNeedUpdate = false;
-        }
-        controls.enabled = true;
-        return;
-    }
-    
-    
-    if( intersects.length !== 0 ){
-        
-        var face = intersects[0].face.clone();
-        geometry = intersects[0].object.geometry;
-        var pointCoords = intersects[0].point;
-        var faceVertices = [];
-        var distance;
-        geometry.vertices.forEach(function(item, i) {
-            if(i == face.a || i == face.b || i == face.c) {
-                var selItem = item.clone();
-                selItem.index = i;
-                faceVertices.push(selItem); 
-            }
-        })
-        faceVertices.forEach(function(item, i) {
-            distance = pointCoords.distanceTo(item);
-            item.distance = distance;
-        })
-        faceVertices.sort(function(a, b) {
-            return a.distance - b.distance;
-        })
-        point = new THREE.Mesh( new THREE.SphereGeometry(0.1), new THREE.MeshBasicMaterial({color: 0x404040}));
-        point.name = 'point';
-        point.position.copy(faceVertices[0]);
-        index = faceVertices[0].index;
-        models.push(point);
-        scene.add(point);
->>>>>>> 2982390d10779708c23688fbbe4c9656f343b843
     }
 }
 
-<<<<<<< HEAD
-//var check;
 function getPoint(modelIntersects) {
     
     var face = modelIntersects[0].face.clone();
@@ -249,16 +164,6 @@ saveButton.onclick = function(e) {
     var parse = save.parse( curModel );
     console.log(parse);
 }
-=======
-// function onDrag(mouse) {
-
-//     mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-//     mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-//     return mouse;
-
-// }
->>>>>>> 2982390d10779708c23688fbbe4c9656f343b843
 
 function render() {
     requestAnimationFrame(render);
